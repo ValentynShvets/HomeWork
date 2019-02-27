c901a8139bbbf2ae9e43329dc196079ecf887a63
Valentyn Shvets <valentynshvets@icloud.com>
2019-02-27 13:21:19 +0200

    Fifteen update

---
+++ fifteen_2.py
@@ -0,0 +1,170 @@
+def print_greeting():
+    print("*" * 5, "Hello", "*" * 5)
+
+
+def size():
+    size = 0
+    while size == 0:
+        size = int(input("Введіть розмір поля>> "))
+        if 3 > size or size > 9:
+            print("Введіть розмір від 3 до 9")
+            size = 0
+    else:
+        return size
+
+
+def initialize_bord(size):
+    """
+
+    :return: list --> [15,14, ...., 3,1,2,0]
+
+    """
+    board = []
+    for item in range(3, size ** 2):
+        board.append(item)
+
+    board = board[::-1]
+    board.append(1)
+    board.append(2)
+    board.append("_")
+    # board = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 1, 2, "_"]
+
+    return board
+
+
+def check(board):
+    """
+
+    :param board:
+    :param bord: [1, 2, 3,...., 14, 15, 0] --> False otherwise True
+    :return: True -- bord is not sorted
+            False -- bord is sorted
+    """
+    winlist = []
+    for win in range(1, size ** 2):
+        winlist.append(win)
+
+    winlist = winlist[::-1]
+    winlist.append("_")
+
+    if board == winlist:
+        print("Ви виграли")
+        return False
+    return True
+
+
+def print_bord(board, size):
+    a = 0
+
+    pass
+    for i in board:
+        print(i, end="\t")
+        a += 1
+        if a == size:
+            print()
+            print("__" * size * 2)
+            a = 0
+
+    return board
+
+
+def move(board, moved, size, imp1, imp2):
+    possition = board.index(moved)
+    space = board.index("_")
+    # a = 0
+    b = possition in imp1
+
+    c = possition in imp2
+    if space - 1 == possition and not b:
+        m = board[space]
+        board[space] = board[possition]
+        board[possition] = m
+        print(f"Число {moved} переміщено")
+
+    elif space + 1 == possition and not c:
+        m = board[space]
+        board[space] = board[possition]
+        board[possition] = m
+        print(f"Число {moved} переміщено")
+
+    elif space - size == possition:
+        m = board[space]
+        board[space] = board[possition]
+        board[possition] = m
+        print(f"Число {moved} переміщено")
+
+    elif space + size == possition:
+        m = board[space]
+        board[space] = board[possition]
+        board[possition] = m
+        print(f"Число {moved} переміщено")
+
+    else:
+        print("Це число перемістити не можна")
+
+    return board
+    # for i in board:
+    #     print(i, end="\t")
+    #     a += 1
+    #     if a == 4:
+    #         print()
+    #         print("_______________")
+    #         a = 0
+    # return board
+
+
+def impossible_move1(size):
+    imp = []
+    for k in range(1, size):
+        imp.append(k * size - 1)
+
+    return imp
+
+
+def impossible_move2(size):
+    impos = []
+    for k in range(1, size):
+        impos.append(k * size)
+
+    return impos
+
+
+def print_file(board, size):
+    x = 0
+    with open("fifteen.txt", "a") as f:
+        for line in board:
+            print(line, file=f, end="\t")
+            x += 1
+            if x == size:
+                f.write("\n")
+                x = 0
+        f.write("__" * size * 2 + "\n")
+
+
+def answer():
+    while True:
+        moved = int(input("Number to move >> "))
+        if 1 <= moved <= 15:
+            return moved
+        else:
+            print("Введіть число від 1 до 15")
+
+
+size = size()
+
+print_greeting()
+
+board = initialize_bord(size)
+
+imp1 = impossible_move1(size)
+
+imp2 = impossible_move2(size)
+
+while check(board):
+    print_bord(board, size)
+    try_move = answer()
+    move(board, try_move, size, imp1, imp2)
+    print_file(board, size)
+    print()
+
+print("Congratulation You have ordered the bord")
