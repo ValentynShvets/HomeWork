16fc957cc6d3196ffb19ebfa852eb640017ab247
Valentyn Shvets <valentynshvets@icloud.com>
2019-03-27 17:05:19 +0200

    scrapped sqlite

--- quotes.py
+++ quotes.py
@@ -1,8 +1,16 @@
 import urllib3
+import sqlite3
 from bs4 import BeautifulSoup
+
+# conn = sqlite3.connect("my.sql")
 http = urllib3.PoolManager()
 url = 'http://quotes.toscrape.com/'
 r = http.request("GET", url)
+conn = sqlite3.connect("my.sql")
+cur = conn.cursor()
+# cur.execute("""CREATE TABLE authors
+# #                   (id, authors)
+# #                """)
 
 tetx = open("crap.txt", "w")
 soup = BeautifulSoup(r.data, 'html.parser')
@@ -12,10 +20,16 @@ url2 = " "
 url = 'http://quotes.toscrape.com/'
 url1 = 'http://quotes.toscrape.com/'
 b = 0
+a = 1
+c = 1
 page = 1
 while b == 0:
+    conn = sqlite3.connect("my.sql")
+    cur = conn.cursor()
+
+
     print("-" * 5, "Page", page, "-" * 5)
-    tetx.write(str("-" * 5) + "Page" + str(page) + str("-" * 5) +"\n")
+    tetx.write(str("-" * 5) + "Page" + str(page) + str("-" * 5) + "\n")
 
     http = urllib3.PoolManager()
     r = http.request("GET", url)
@@ -25,14 +39,50 @@ while b == 0:
     authors = soup.find_all("div", attrs={"class": "quote"})
 
     for i, quote in enumerate(rez):
+
         main_text = quote.find("span", attrs={'class': "text"})
         author_text = authors[i].find("small", attrs={'class': "author"})
         print(i + 1, main_text.text[1:-1], "by", author_text.text)
-        tetx.write(str(i + 1 ) + " " + main_text.text[1:-1] + " by "+ author_text.text + "\n")
+        tetx.write(str(i + 1) + " " + main_text.text[1:-1] + " by " + author_text.text + "\n")
+
+
+
+        # if author_text.text
+        cur.execute("SELECT id FROM authors WHERE authors = ?", (author_text.text,))
+        data = cur.fetchone()
+
+        if data is None:
+            cur.execute('insert into authors values (%d,"%s");'
+                        % (a, author_text.text))
+            cur.execute("select * from authors")
+            a += 1
+            print(cur.fetchall())
+
+            # cur.execute('insert into phrases values id_authors = ?', (a,))
+
+
+        else:
+            # print(cur.fetchall())
+            pass
+            # cur.execute(f'insert into authors values ({a},{author_text.text})')
+
+        cur.execute("SELECT id FROM authors WHERE authors = ?", (author_text.text,))
+        id_author = cur.fetchone()
+        print(id_author[0])
+
+
+
+        cur.execute('insert into phrases values(%d, %d, "%s");'
+                    %(c, id_author[0], main_text.text[1:-1]))
+        cur.execute("select * from phrases")
+        c+=1
+        print(cur.fetchall())
+        conn.commit()
+
 
     for next in soup.find_all("li", attrs={"class": "next"}):
         for next1 in next.find_all("a"):
-            tt = next.find_all('a', href = True)
+            tt = next.find_all('a', href=True)
             url2 = next1.get('href')
             url = url1 + url2[1::]
             page += 1
@@ -40,3 +90,5 @@ while b == 0:
         b = 1
         print("All page scraped")
         tetx.close()
+
+
